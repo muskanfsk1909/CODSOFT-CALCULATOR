1496e7de74618a37b92598db66dbfcc6345db550
muskanfsk1909 <muskanfsk1909@gmail.com>
2024-06-23 10:35:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// let string = "";
-// let buttons = document.querySelectorAll('.button');
-// Array.from(input).forEach((button)=> {
-//     button.addEventListener('click', (e)=> {
-//         if(e.target.innerHTML == '='){
-//             string = eval(string);
-//             document.querySelector('input').value = string;
-//         }
-//         else if (e.target.innerHTML == 'C'){
-//             string=""
-//             document.querySelector('input').value=string;
-//         }
-//         else{
-//             console.log(e.target)
-//             string=string + e.target.innerHTML;
-//             document.querySelector('input').value = string;
-//         }
-//     })
-// })
-
-
 document.addEventListener('DOMContentLoaded', (event) => {
     const display = document.querySelector('.display input');
     const buttons = document.querySelectorAll('input[type="button"]');
